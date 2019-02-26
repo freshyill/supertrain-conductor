2fdac421fc0910efb070183b801b7d418c30fe30
Chris Coleman <freshyill@gmail.com>
2019-02-26 12:24:18 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,20 @@
+/*
+   This is a super basic service worker based on Remy Sharp's "The copy & paste
+   guide to your first Service Worker."
+
+   There are four URLs below that correspond to the placeholder pages included
+   in the project. Remove them if they don't work for you and add your own.
+
+   Here are some other useful resources to get started with service workers:
+
+   * https://remysharp.com/2016/03/22/the-copy--paste-guide-to-your-first-service-worker
+   * https://css-tricks.com/serviceworker-for-offline/
+   * https://googlechrome.github.io/samples/service-worker/basic/
+   * https://deanhume.com/create-a-really-really-simple-offline-page-using-service-workers/
+   * https://gist.github.com/adactio/fbaa3a5952774553f5e7
+   * https://serviceworke.rs/
+*/
+
 // we'll version our cache (and learn how to delete caches in
 // some other post)
 const cacheName = 'v1::static';
